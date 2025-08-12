c786f4d8be1a3901efad8c3c68b77df68540514e
Tu Nombre <you@example.com>
2025-08-12 18:52:01 +0200

    second commit

--- src/license_server.cpp
+++ src/license_server.cpp
@@ -11,6 +11,13 @@ std::string load_file(const std::string& path) {
 
 int main() {
     httplib::Server svr;
+    svr.Options("/license", [](const httplib::Request&, httplib::Response& res){
+        res.set_header("Access-Control-Allow-Origin", "*");
+        res.set_header("Access-Control-Allow-Methods", "POST, OPTIONS");
+        res.set_header("Access-Control-Allow-Headers", "*");
+        res.status = 204;
+    });
+
     svr.Post("/license", [](const httplib::Request& req, httplib::Response& res){
         auto cid = req.get_param_value("content_id");
         std::string key_data = load_file(cid + ".key");
@@ -19,9 +26,11 @@ int main() {
             res.set_content("missing key", "text/plain");
             return;
         }
+        res.set_header("Access-Control-Allow-Origin", "*");
+        res.set_header("Access-Control-Allow-Headers", "*");
         res.set_content(key_data, "application/octet-stream");
     });
     std::cout << "License server running on http://localhost:8080\n";
-    svr.listen("0.0.0.0", 8080);
+    svr.listen("127.0.0.1", 8080);
     return 0;
 }
